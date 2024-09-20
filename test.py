067575fdf215265cb4c1196ab836d4394ccd2d7b
Cowelist <Henry8bombomdog@gmail.com>
2024-09-20 08:24:24 +0200

    test of design
    Using pygame

---
+++ test.py
@@ -0,0 +1,32 @@
+import pygame
+import sys
+
+# #paint = 1
+# paints = ["Hei", "på", "Deg"]
+
+# for paint in paints:
+#     print (paint)
+
+
+# size = 5 
+
+# for i in range(5):
+#         print("█" * 10)
+
+
+# import tkinter as tk
+
+# window = tk.Tk()
+# window.title("My GUI")
+
+# label = tk.Label(text="Hello, Tkinter!")
+# label.pack()
+
+# button = tk.Button(text="Click me!")
+# button.pack()
+
+# window.mainloop()
+
+
+
+
