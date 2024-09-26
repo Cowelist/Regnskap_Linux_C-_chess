39a90615d083e1503737572d2fd4faf1d2d3b77d
Cowelist <Henry8bombomdog@gmail.com>
2024-09-26 08:34:12 +0200

    added pygame

--- chess.py
+++ chess.py
@@ -1,1 +1,22 @@
-print("\033[34m █\033[0m")
+#!/bin/python
+import pygame
+import os
+
+os.system ("clear")
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.dispaly.set_caption("Chess")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+screen.fill ((225, 255, 255))
+
+
+
+
+#print("\033[34m █\033[0m")
