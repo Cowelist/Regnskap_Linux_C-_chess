2531520a823b89b3b85fad8f245bedbd5106fdfc
Cowelist <Henry8bombomdog@gmail.com>
2024-09-27 09:38:42 +0200

    add class

--- chess.py
+++ chess.py
@@ -10,13 +10,49 @@ pygame.display.set_caption("Chess")
 
 clock = pygame.time.Clock()
 
+x = 100
+y = 100
+
+white = (255, 255, 255)
+
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+object = (0, 0)
 
-screen.fill ((225, 255, 255))
+
+
+class Pice:
+    def __init__(self, position, side):
+        self.position = position
+        self.__side = None
+        #if side = true | side = white
+
+   
+    def movment(self, movment):
+        if self.__side == True:
+            pass #+1 cord
+        elif self.__side == False:
+            pass #-1 cord
+
+
+class Pawn(Pice):
+    def set_side (self, side):
+        self.__side = None
+
+
+
+
+
+       
+screen.fill (white)
+
+screen.blit(x,y)
+
+pygame.display.flip()
 
 clock.tick(60)
 
