534e516ce4362fd7949c9735c6c30df27d2fa9cd
Cowelist <Henry8bombomdog@gmail.com>
2024-11-04 10:30:09 +0100

    Testing C++

---
+++ test/test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+    int num1 = 10;
+    int num2 = 10;
+    if (num1 == num2){ 
+        printf("Hei");
+    }   else {
+        printf("nothing");
+    }
+    return 5;
+}
+// if (10 == 10){
+//     printf("fungerte");
+//     return 0;
+// }
+
+
+// int main(){
+//     printf("Hello BOB!");
+//     return 1;
+// }
